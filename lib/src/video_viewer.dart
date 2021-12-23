34a9fb1da612f7b9361bc6c5b14ee6d91a2107fb
tridarid <tridianto.putra@live.com>
2021-12-23 13:31:17 +0700

    remove warning in video viewer

--- lib/src/video_viewer.dart
+++ lib/src/video_viewer.dart
@@ -77,8 +77,8 @@ class _VideoViewerState extends State<VideoViewer> {
                       child: FittedBox(
                         fit: BoxFit.cover,
                         child: SizedBox(
-                          width: _controller.value.size.width ?? 0,
-                          height: _controller.value.size.height ?? 0,
+                          width: _controller.value.size.width,
+                          height: _controller.value.size.height,
                           child: VideoPlayer(_controller),
                         ),
                       ),
